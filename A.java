040c0959b0bef87acf74de9f0b3b882136de6e73
zhangsan <zhangsan@itcast.cn>
2019-08-31 20:52:50 +0800

    第一次修改

---
+++ A.java
@@ -0,0 +1,24 @@
+public class A {
+	
+	public class void main(String[] args){
+		sout("xiaohongxiaohong");
+		sout("xiaohongxiaohong");
+		sout("xiaohongxiaohong");
+		sout("xiaohongxiaohong");
+		sout("xiaohongxiaohong");
+		sout("xiaohongxiaohong");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		sout("weilai");
+		
+			
+	}
+	
+}
